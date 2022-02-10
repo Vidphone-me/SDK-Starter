218b6c1db6e8587afe73d124dad3c1763793bdae
akil <akil@everycrave.me>
2022-02-10 12:17:32 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,213 @@
+var fs = require("fs");
+var host = "127.0.0.1";
+var port = 8001;
+var express = require("express");
+var request = require("request");
+
+var app = express();
+var bodyParser = require('body-parser');
+
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public")); //use static files in ROOT/public folder
+
+
+const ORGANIZATION_ID = "6080207c-1a4e-4fd4-b83b-c82d837e58d8";
+const API_KEY = "BnC6nbld.NdzTl1oTa8cUP8HkLtu9DrJZoEjve15w";
+const ROOM_TITLE = "abcd";
+
+
+var bodyParser = require("body-parser");
+const { http } = require("http");
+app.use(bodyParser.json());
+
+
+// get the roomname, create it if it doesn't exist
+app.post("/getRoomName", function(req, res) {
+    //set url
+    let url = 'https://developers.vidphone.me/meetings/list';
+
+    //set header
+    var headers = {
+        'Authorization': 'Api-Key '+API_KEY,
+    };
+
+    //set request parameter
+    request.post({headers: headers, url: url, form: {}, method: 'POST'}, function (e, r, body) {
+        var bodyValues = JSON.parse(body);
+        if(bodyValues.meetings){
+            let meetings = bodyValues.meetings;
+            let roomname = null;
+            meetings.forEach(function(meeting){
+                if(meeting.title == ROOM_TITLE){
+                    roomname = meeting.roomname;
+                }
+            })
+
+            if(roomname){
+                return res.send({"roomname":roomname});
+            }
+
+
+            request.post({headers: headers, url: "https://developers.vidphone.me/meetings/create", form: {
+                "organization_id": ORGANIZATION_ID,
+                "title": ROOM_TITLE,
+            }, method: 'POST'}, function (e, r, meetingObj) {
+                var obj = JSON.parse(meetingObj);
+                return res.send(obj)
+            });
+
+        }
+    });
+})
+
+
+
+
+
+// create a participant for the iframe user
+app.post("/createParticipant", function(req, res) {
+
+    let roomname = req.body.roomname || "";
+    let name = req.body.name || "";
+    let user_id = req.body.user_id || "";
+    let is_host = req.body.is_host || false;
+
+    //set url
+    let url = 'https://developers.vidphone.me/participants/create';
+
+    //set header
+    var headers = {
+        'Authorization': 'Api-Key '+API_KEY,
+    };
+
+    var data = {
+        "organization_id": ORGANIZATION_ID,
+        roomname: roomname,
+        name: name, 
+        user_id: user_id,
+        is_host: is_host
+    }
+
+    //set request parameter
+    request.post({headers: headers, url: url, form: data, method: 'POST'}, function (e, r, body) {
+        var obj = JSON.parse(body);
+        return res.send(obj)
+    });
+})
+
+
+
+
+
+app.post("/createWebinar", function(req, res) {
+    let title = req.body.title || "";
+    let description = req.body.description || "";
+    let image_url = req.body.image_url || ""; //optional
+    let start_day = req.body.start_day || ""; //optional
+    let start_month = req.body.start_month || ""; //optional
+    let start_year = req.body.start_year || ""; //optional
+    let start_hour = req.body.start_hour || ""; //optional
+    let start_minute = req.body.start_minute || ""; //optional
+    let fb_key = req.body.fb_key || ""; //optional
+    let yt_key = req.body.yt_key || ""; //optional
+    let duration = req.body.duration || 60; //optional
+
+    //set url
+    let url = 'https://developers.vidphone.me/webinars/create';
+
+    //set header
+    var headers = {
+        'Authorization': 'Api-Key '+API_KEY,
+    };
+
+    var data = {
+        "organization_id": ORGANIZATION_ID,
+        title: title, 
+        description: description,
+        image_url: image_url,
+        start_day: start_day,
+        start_month: start_month,
+        start_year: start_year,
+        start_hour: start_hour,
+        start_minute: start_minute,
+        fb_key: fb_key,
+        yt_key: yt_key,
+        duration: Number(duration)
+    }
+
+    //set request parameter
+    request.post({headers: headers, url: url, form: data, method: 'POST'}, function (e, r, body) {
+        var obj = JSON.parse(body);
+        return res.send(obj)
+    });
+})
+
+
+
+
+
+app.post("/createPresenter", function(req, res) {
+    let name = req.body.name || "";
+    let profile_img_url = req.body.profile_img_url || ""; //optional
+    let room = req.body.room || "";
+    let user_id = req.body.user_id || ""; //optional
+
+    //set url
+    let url = 'https://developers.vidphone.me/webinars/create-presenter';
+
+    //set header
+    var headers = {
+        'Authorization': 'Api-Key '+API_KEY,
+    };
+
+    var data = {
+        "organization_id": ORGANIZATION_ID,
+        name: name, 
+        profile_img_url: profile_img_url,
+        room: room,
+        user_id: user_id,
+    }
+
+    //set request parameter
+    request.post({headers: headers, url: url, form: data, method: 'POST'}, function (e, r, body) {
+        var obj = JSON.parse(body);
+        return res.send(obj)
+    });
+})
+
+
+
+app.post("/createSubscriber", function(req, res) {
+    let name = req.body.name || "";
+    let room = req.body.room || "";
+    let user_id = req.body.user_id || ""; //optional
+
+    //set url
+    let url = 'https://developers.vidphone.me/webinars/create-subscriber';
+
+    //set header
+    var headers = {
+        'Authorization': 'Api-Key '+API_KEY,
+    };
+
+    var data = {
+        "organization_id": ORGANIZATION_ID,
+        name: name, 
+        room: room,
+        user_id: user_id,
+    }
+
+    //set request parameter
+    request.post({headers: headers, url: url, form: data, method: 'POST'}, function (e, r, body) {
+        var obj = JSON.parse(body);
+        return res.send(obj)
+    });
+})
+
+
+
+app.listen(port, host);
---
+++ public/main.js
@@ -0,0 +1,175 @@
+let roomname = null;
+let participant_id = null;
+
+
+
+// populate the iframe url, unhide the iframe and start the call
+function startCall(){
+    let iframe = document.getElementById("iframe");
+    iframe.src = "https://vidphone.me/iframe_room/"+roomname+"/"+participant_id;
+    iframe.classList.remove("hidden");
+}
+
+
+// create a participant via the API
+function createParticipant(name){
+    let user_id = new Date().getTime();
+
+    $.post("/createParticipant", {name: name,roomname:roomname, user_id:user_id, is_host:false}, function(data) { 
+        if(data && data.error){
+            alert(data.msg);
+            return;
+        }
+        participant_id = data.participant_id;
+        startCall();
+    })
+}
+
+
+
+// on form submit get the roomname and create a participant
+$("#start-meeting-form").on("submit", function(event) {
+    event.preventDefault();
+    let name = $("#name-input").val();
+
+    if(!name || name == "" || name.trim() == "") {
+        return;
+    }
+
+    $.ajax({
+        url: "/getRoomName",
+        type: "POST",
+        data: {name: name},
+        dataType: 'json', // lowercase is always preferered though jQuery does it, too.
+        success: function(data){
+            roomname = data.roomname;
+            console.log(roomname);
+            // create a participant
+            createParticipant(name);
+        },error: function(error){
+            // alert(error);
+        }
+    });
+    return false;
+});
+
+
+
+
+$("#start-webinar-form").on("submit", function(event) {
+    event.preventDefault();
+    let title = $("#webinar-title-input").val();
+    let description = $("#webinar-desc-input").val();
+    let duration = 60;
+
+    if(!title || title == "" || title.trim() == "") {
+        return;
+    }
+
+    $.ajax({
+        url: "/createWebinar",
+        type: "POST",
+        data: {title: title, description:description},
+        dataType: 'json', // lowercase is always preferered though jQuery does it, too.
+        success: function(data){
+            console.log(data);
+            let room = data.room;
+            showWebinarOptionsForRoom(room);
+        },error: function(error){
+            // alert(error);
+        }
+    });
+    return false;
+});
+
+
+
+
+
+$("#presenter-webinar-form").on("submit", function(event) {
+    event.preventDefault();
+    let room = $("#presenter-webinar-room").val();
+    let name = $("#presenter-name").val();
+
+    if(!room || room == "" || room.trim() == "") {
+        return;
+    }
+
+    $.ajax({
+        url: "/createPresenter",
+        type: "POST",
+        data: {name:name, room:room},
+        dataType: 'json', // lowercase is always preferered though jQuery does it, too.
+        success: function(data){
+            let id = data.id;
+
+            let iframe = document.getElementById("iframe");
+            iframe.src = "https://test1.vidphon.com/iframe_presenter/"+room+"/"+id;
+            iframe.classList.remove("hidden");
+        },error: function(error){
+            // alert(error);
+        }
+    });
+    return false;
+});
+
+
+
+
+
+
+$("#subscriber-webinar-form").on("submit", function(event) {
+    event.preventDefault();
+    let room = $("#subscriber-webinar-room").val();
+    let name = $("#subscriber-name").val();
+
+    if(!room || room == "" || room.trim() == "") {
+        return;
+    }
+
+    $.ajax({
+        url: "/createSubscriber",
+        type: "POST",
+        data: {name:name, room:room},
+        dataType: 'json', // lowercase is always preferered though jQuery does it, too.
+        success: function(data){
+            let id = data.id;
+
+            let iframe = document.getElementById("iframe");
+            iframe.src = "https://test1.vidphon.com/iframe_subscriber/"+room+"/"+id;
+            iframe.classList.remove("hidden");
+        },error: function(error){
+            // alert(error);
+        }
+    });
+    return false;
+});
+
+
+
+
+
+
+
+
+function showWebinarOptionsForRoom(room){
+    $("#start-webinar-form").hide();
+    $("#webinar-label").hide();
+    $("#webinar-room-span").val(room);
+    $("#webinar-form-1").show();
+}
+
+
+
+$("#copy-room-btn").on("click", function(event) {
+    event.preventDefault();
+    let room = $("#webinar-room-span").val();
+
+    $("#copy-room-btn").text("Room copied!");
+
+    setTimeout(()=>{
+        $("#copy-room-btn").text("Copy Room");
+    },3000);
+    navigator.clipboard.writeText(room);
+    return false;
+})
